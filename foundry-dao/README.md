a3d1cdc47f4fd4434d0667ab619dc019572f3462
Sylvain TAGNABBOU <sylvaintagnabou@gmail.com>
2024-06-15 17:37:25 +0000

    (done) -> foundry DAO

--- foundry-dao/README.md
+++ foundry-dao/README.md
@@ -64,3 +64,8 @@ $ forge --help
 $ anvil --help
 $ cast --help
 ```
+
+## Here are our main objectives:
+1. Establish a contract completely controlled by our DAO.
+2. Every transaction the DAO wants to send will need to be voted on.
+3. For voting, we'll utilize ERC 20 tokens.
---
+++ foundry-dao/test/MyGovernorTest.t.sol
@@ -0,0 +1,100 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MyGovernor} from "../src/MyGovernor.sol";
+import {GovToken} from "../src/GovToken.sol";
+import {TimeLock} from "../src/TimeLock.sol";
+import {Box} from "../src/Box.sol";
+
+contract MyGovernorTest is Test {
+    MyGovernor governor;
+    GovToken govToken;
+    TimeLock timelock;
+    Box box;
+
+    address USER = makeAddr("USER");
+    uint256 constant INITIAL_SUPPLY = 100 ether;
+
+    uint256 constant MIN_DELAY = 3600;
+    uint256 constant VOTING_DELAY = 1;
+    uint256 constant VOTING_PERIOD = 50400;
+    address[] proposers;
+    address[] executors;
+    uint256[] values;
+    bytes[] calldatas;
+    address[] targets;
+
+    function setUp() public {
+        govToken = new GovToken();
+        govToken.mint(USER, INITIAL_SUPPLY);
+
+        vm.startPrank(USER);
+        govToken.delegate(USER);
+
+        timelock = new TimeLock(1, proposers, executors);
+        governor = new MyGovernor(govToken, timelock);
+
+        bytes32 proposerRole = timelock.PROPOSER_ROLE();
+        bytes32 executorRole = timelock.EXECUTOR_ROLE();
+        bytes32 adminRole = timelock.TIMELOCK_ADMIN_ROLE();
+
+        timelock.grantRole(proposerRole, address(governor));
+        timelock.grantRole(executorRole, address(0));
+        timelock.revokeRole(adminRole, USER);
+        vm.stopPrank();
+
+        box = new Box();
+        box.transferOwnership(address(timelock));
+    }
+
+    function testCantUpdateBoxWithoutGovernance() public {
+        vm.expectRevert();
+        box.store(42);
+    }
+
+    function testGovernanceUpdateBox() public {
+        uint256 value = 42;
+        string memory description = "Store 42 in the box";
+        bytes memory encodedFunctionCall = abi.encodeWithSignature("store(uint256)", value);
+        values.push(0);
+        calldatas.push(encodedFunctionCall);
+        targets.push(address(box));
+
+        // 1. Propose to the DAO
+        uint256 proposalId = governor.propose(targets, values, calldatas, description);
+
+        // View the state of the proposal
+        console.log("Proposal state: ", (uint256(governor.state(proposalId))));
+
+        vm.warp(block.timestamp + VOTING_DELAY + 1);
+        vm.roll(block.number + VOTING_DELAY + 1);
+
+        console.log("Proposal state: ", (uint256(governor.state(proposalId))));
+
+        // 2. Vote on the proposal
+        string memory reason = "I like the proposal";
+        uint8 voteWay = 1; // 1 for yes
+        vm.prank(USER);
+        governor.castVoteWithReason(proposalId, voteWay, reason);
+
+        vm.warp(block.timestamp + VOTING_PERIOD + 1);
+        vm.roll(block.number + VOTING_PERIOD + 1);
+
+        // 3. Queue the proposal
+        bytes32 descriptionHash = keccak256(abi.encodePacked(description));
+        governor.queue(targets, values, calldatas, descriptionHash);
+
+        vm.warp(block.timestamp + MIN_DELAY + 1);
+        vm.roll(block.number + MIN_DELAY + 1);
+
+        // 4. Execute the proposal
+        governor.execute(targets, values, calldatas, descriptionHash);
+
+
+        console.log("Box value: ", box.retrieve());
+        assertEq(box.retrieve(), value);
+
+    }
+}
